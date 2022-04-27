2813c8e3690a51dd545aa1c72a2ba2dc5bac9c4b
Nadezhda Yanushevich <nadiya_23@rambler.ru>
2022-04-28 01:58:28 +0300

    feat: solve What season task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let arr = [];
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for (let i = 0; i < members.length; i += 1) {
+    if (typeof members[i] === "string") {
+      arr.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  arr.sort();
+  if (arr.length === 0) {
+    return false;
+  }
+  return arr.join("");
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  console.log(date);
+  // console.log(Object.getOwnPropertySymbols(date).length > 0);
+
+  if (Date.parse(date) === NaN) {
+    return "Invalid date!";
+  } else if (getSeason.arguments.length === 0) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (Object.getOwnPropertySymbols(date).length > 0) {
+    throw new Error("Invalid date!");
+  }
+
+  try {
+    date.getMonth();
+  } catch {
+    throw new Error("Invalid date!");
+  }
+
+  let month = date.getMonth();
+
+  if (2 <= month && month < 5) {
+    return "spring";
+  } else if (5 <= month && month < 8) {
+    return "summer";
+  } else if (8 <= month && month < 11) {
+    return "autumn";
+  } else {
+    return "winter";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
